b9c7ed53b78b376e5321342d0d524f8add33cece
unknown <Administrator@celab2-2a.comp1-domain.spit.ac.in>
2012-09-16 15:29:34 +0530

    last commit

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,14 @@
 class Company < ActiveRecord::Base
+  validate :price_should_be_positive
+  def price_should_be_positive
+    if price < 0
+      errors.add :price,'should be positive'
+    end
+  end
+  validate :symbol_should_be_small
+  def symbol_should_be_small
+    if symbol.length <3 or symbol.length >4
+      errors.add :symbol,'should be three or four lettered'
+    end
+  end
 end
