b8718c5221d691f6e53220599ded0f1d2648197d
unknown <Administrator@celab2-2a.comp1-domain.spit.ac.in>
2012-09-17 19:11:44 +0530

    last day

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,5 +1,13 @@
 class Investment < ActiveRecord::Base
+  belongs_to :portfolio
+  belongs_to :company
   validate :quantity_should_be_positive
+  def    worth_at_cost
+    return quantity*cost
+  end
+  def worth_today
+    return quantity*company.price
+  end
   def quantity_should_be_positive
     if quantity < 0
       errors.add :quantity,'should be positive'
